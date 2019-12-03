8de6e01ee1e7f147a1c981d0e4b4b7931c344702
zoltan.kada <zoltan.kada@edreamsodigeo.com>
2019-12-03 15:42:39 +0100

    moved html and css to DOCS

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     devtool: 'none',
     module: {
